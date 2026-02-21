117999e381458acadd5218cc87145741a9ad6bb8
Webeside Technology <webesidetechnologyindia@gmail.com>
2026-02-21 20:49:42 +0530

    update

--- components/LocationContact.tsx
+++ components/LocationContact.tsx
@@ -56,7 +56,7 @@ export default function LocationContact() {
               </a>
             </div>
 
-            <div className="flex gap-4 pt-4 ml-[76px] md:ml-[0px]">
+            <div className="flex gap-4 pt-4 ml-[0px] md:ml-[0px]">
               
               <a href="https://www.facebook.com/people/Eurokidssec86summerpalm/61587151854295/" aria-label="Facebook" className="w-11 h-11 bg-[#253b80] hover:bg-[#1877F2] text-white rounded-lg flex items-center justify-center transition-all duration-300 shadow-md hover:-translate-y-1">
                 <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24"><path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"></path></svg>
