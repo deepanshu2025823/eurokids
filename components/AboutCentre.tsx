af345b0b967262afa80fc657dcd3ee3535296b2d
Webeside Technology <webesidetechnologyindia@gmail.com>
2026-02-22 19:54:36 +0530

    update

--- components/AboutCentre.tsx
+++ components/AboutCentre.tsx
@@ -41,7 +41,7 @@ const features = [
     bgColor: "bg-blue-100/50"
   },
   {
-    name: "School Bus Service",
+    name: "Transport Service With Nanny",
     icon: (
       <svg className="w-5 h-5 text-orange-500" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24">
         <path strokeLinecap="round" strokeLinejoin="round" d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4" />
