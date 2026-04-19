1590b1c2617ceb0b637e361ee959d59b9c77da11
user <user@A-S-F109.local>
2026-04-19 17:13:15 +0530

    update

--- components/LocationContact.tsx
+++ components/LocationContact.tsx
@@ -24,7 +24,7 @@ export default function LocationContact() {
                 <svg className="w-6 h-6 md:w-7 md:h-7" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"></path></svg>
               </div>
               <p className="text-gray-700 font-semibold text-[15px] md:text-lg">
-                Monday to Saturday | 09:30 AM to 05:00 PM
+                Monday to Saturday | 09:00 AM to 06:00 PM
               </p>
             </div>
 
